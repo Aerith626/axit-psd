f163759272f52c8fc379137d1322fcbdaa145b3d
Aerith <joltzer02@gmail.com>
2022-03-29 16:11:11 -0400

    Agregados tabs funcionales, padding agregado a row tabs. Falta anadir space-between

---
+++ script/script.js
@@ -0,0 +1,30 @@
+
+
+function actTabs () {
+    document.querySelectorAll('.tabs__btn').forEach(btn => {
+        btn.addEventListener('click', ()=> {
+            let sideBar = btn.parentElement;
+            let tabsContainer = sideBar.parentElement;
+            let tabsNum = btn.dataset.btnTab;
+            let tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabsNum}"]`);
+            sideBar.querySelectorAll('.tabs__btn').forEach(btn => {
+                btn.classList.remove('tabs__btn--active');
+            });
+            tabsContainer.querySelectorAll('.tabs__content').forEach(tab => {
+                tab.classList.remove('tabs__content--active');
+            });
+
+            btn.classList.add('tabs__btn--active');
+            tabToActivate.classList.add("tabs__content--active");
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    actTabs ();
+
+    document.querySelectorAll('.tabs').forEach(tabsContainer => {
+        tabsContainer.querySelector('.tabs__sidebar .tabs__btn').click();
+    });
+
+});
